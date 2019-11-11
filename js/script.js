bc2b066c5420474f22a47e7b8c03e4e89b449de5
wallzack <vulcheek@gmail.com>
2019-11-11 08:08:44 +0100

    Add script to remove active class

--- js/script.js
+++ js/script.js
@@ -12,10 +12,23 @@ document.getElementById('test-button').addEventListener('click', function(){
     console.log(event);
   
     /* remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }   
+
   
     /* get 'href' attribute from the clicked link */
   
